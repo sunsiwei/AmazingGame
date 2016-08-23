3996d996135202ea2f5a202264cd6a33b7c8efd4
sunsiwei <1064874807@qq.com>
2016-08-23 16:30:23 +0800

    8.23_company

--- Assets/Scripts/Systems/PaySystem.cs
+++ Assets/Scripts/Systems/PaySystem.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace PacmanGame
 {
+    public class WaresData
+    {
+        public string name = "";
+        public int id = 0;
+        public int diamondNum = 0;
+    }
     public class PaySystem : SystemBase
     {
+        Dictionary<int, WaresData> waresList;
+
         public static string name = "PaySystem";
 
         public PaySystem(string _name)
@@ -21,7 +30,13 @@ namespace PacmanGame
         public override void Create()
         {
             base.Create();
+            InitWarsesList();
 
+        }
+
+        public Dictionary<int, WaresData> GetWaresList()
+        {
+            return waresList;
         }
 
         public void Pay()
@@ -40,11 +55,37 @@ namespace PacmanGame
                 price
                 );
         }
+        // 0 支付失败， 其他数字是商品id
         public void PayReply(string str)
         {
             int code = int.Parse(str);
+            if (code == 0)
+            {
+                // pay fail
+            }
+            else
+            {
+                DiamondSystem ds = SystemManager.Instance.GetSystem(DiamondSystem.name) as DiamondSystem;
+                ds.AddDiamond(waresList[code].diamondNum);
+            }
+
             if (EventPayReply != null)
                 EventPayReply(code);
+        }
+
+        void InitWarsesList()
+        {
+            waresList = new Dictionary<int, WaresData>();
+            WaresData a = new WaresData();
+            a.name = "一小包宝石";
+            a.id = 1;
+            a.diamondNum = 5;
+            waresList.Add(1, a);
+            WaresData b = new WaresData();
+            b.name = "一大包宝石";
+            b.id = 2;
+            b.diamondNum = 10;
+            waresList.Add(2, b);
         }
 
     }
---
+++ Assets/Scripts/UI/WaresListPage.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace PacmanGame
+{
+    public class WaresListPage : PageBase
+    {
+        public WaresListPage(UIHierarchy _hierarchy, string _path)
+			:base(_hierarchy, _path)
+		{
+			
+		}
+
+        Transform[] wareItem;
+		protected override void Awake(GameObject go)
+		{
+			Button btnClose = transform.Find("BtnClose").GetComponent<Button>();
+			btnClose.onClick.AddListener(OnBtnClose);
+
+            InitWareItem();
+		}
+
+        protected override void Refresh()
+        { 
+            
+        }
+        void InitWareItem()
+        {
+            
+        }
+        void OnBtnClose()
+        {
+            Hide();
+        }
+    }
+}
+
